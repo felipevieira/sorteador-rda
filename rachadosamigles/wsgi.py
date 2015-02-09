313c3e43414bcc4a4c668a228522e17f6dbd9d4b
Felipe Vieira <felipe29vieira@gmail.com>
2015-02-09 10:26:14 -0300

    wsgi

--- rachadosamigles/wsgi.py
+++ rachadosamigles/wsgi.py
@@ -1,7 +1,14 @@
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+"""
+WSGI config for rachadosamigles project.
 
-# This application object is used by the development server
-# as well as any WSGI server configured to use this file.
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rachadosamigles.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
