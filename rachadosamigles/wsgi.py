40e403866a23cc052515ee4183327e7e1fb3f0d0
Felipe Vieira <felipe29vieira@gmail.com>
2015-02-09 21:24:06 -0300

    teste

--- rachadosamigles/wsgi.py
+++ rachadosamigles/wsgi.py
@@ -12,7 +12,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rachadosamigles.settings")
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
-from whitenoise.django import DjangoWhiteNoise
 
 application = Cling(get_wsgi_application())
-application = DjangoWhiteNoise(application)
