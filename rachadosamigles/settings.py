666d3868d39e5c7d0149ed5252d66b81ded4fbeb
Felipe Vieira <felipe29vieira@gmail.com>
2015-02-09 21:28:01 -0300

    teste

--- rachadosamigles/settings.py
+++ rachadosamigles/settings.py
@@ -95,8 +95,8 @@ TEMPLATE_DIRS = (
 )
 
 # Parse database configuration from $DATABASE_URL
-#import dj_database_url
-#DATABASES['default'] =  dj_database_url.config()
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -113,5 +113,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
