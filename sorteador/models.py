6a37f04da6f717d8b1c1b652ce6fdd4b54fefe61
Felipe Vieira <felipe29vieira@gmail.com>
2015-03-08 23:10:45 -0300

    changing image mechanism

--- sorteador/models.py
+++ sorteador/models.py
@@ -2,10 +2,10 @@
 from django.db import models
 
 class Jogador(models.Model):
+	username = models.CharField(max_length=100)
 	nome = models.CharField(max_length=100)
 	apelido = models.CharField(null=True, max_length=100)
 	eh_diretoria = models.BooleanField(default=False)
-	foto = models.ImageField(upload_to="atletas/", null=True, blank=True)
 
 	def __unicode__(self):
 		return u"%s" % (self.nome)
@@ -17,6 +17,7 @@ class Equipe(models.Model):
 	anticabeca = models.ForeignKey("Jogador", null=True, related_name="anticabeca")
 	aspira = models.ForeignKey("Jogador", null=True, related_name="aspira")
 	normais = models.ManyToManyField(Jogador, null=True, related_name="normais")
+	vitorias = models.IntegerField(null=True, blank=True)
 
 	def __unicode__(self):
 		return u"%s - %s" % (self.nome, self.data_criacao)
